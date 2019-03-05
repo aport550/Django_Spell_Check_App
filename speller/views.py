877bcc1721e8ac8a35dd94c1f1bb520edf2d3787
Allan Porter <aport550@gmail.com>
2019-03-05 01:15:22 -0500

    Changed Functionality of app and fixed bugs

--- speller/views.py
+++ speller/views.py
@@ -5,63 +5,68 @@ from django.template import loader
 import os
 from django.conf import settings
 
+#define the txt file we're using as our wordlist as "dictionary"
 dictionary = open(os.path.join(settings.BASE_DIR, 'wordlist.txt'))
 
+#landing page server
 def home(request):
 	return render(request, 'home.html')
 
+#spellchecked page server
 def checked(request):
     if request.method == "POST":
-        this_File = request.FILES['this_file'].read() # get the uploaded file
+        this_File = request.FILES['this_file'].read() # get our file that user uploaded
         thisFile = this_File.decode("utf-8")
         dictionary = 'wordlist.txt'
 
-        d = []
+        d = [] #empty list of words to be later used as dictionary
         with open(dictionary) as f:
         	for word in f:
-        		word = word[:-1]
+        		word = word[:-1] #format words
         		word = word.lower()
-        		d.append(word)
-        words_list = []
+        		d.append(word) #add all words from wordlist file to dictionary
 
-        	#g = g.read() 
-        line = thisFile.split(" ")
+        #add these to dict so they don't come up as mispelled
+        endlines = ["\n", "\t", '', "\n\n", "\n\n\n"]
+        symbols = [".", "?", "_", "-", "!", "+", "=", "(", ")", "[", "]", ":", ";", "..."]
 
-        capital_words = []
-        for l in line:
-        	first_char = (l[0])
-        	if first_char.isupper():
-        		l = l.lower()
-        		capital_words.append(l)
+        for line in endlines:
+        	d.append(line)
+        for symbol in symbols:
+        	d.append(symbol)
+
+        words_list = [] #This is the empty list of words from the file user uploaded
+
+        temp_list = thisFile.split(" ") #create a list of users words
+
+        #check for capitals so we can check if base word is mispelled
+        for l in temp_list:
+        	if l.istitle():
+        		lower_case = l.lower()
+        		if lower_case in d:
+        			d.append(l)
+        		else:
+        			pass
         	words_list.append(l)
 
-        count = 0
-        lwos = []
-
+        #check if words contain endlines or symbols so we can check if base word is mispelled
         for w in words_list:
-        	if w[len(w)-1] == ".":
-        		this_word = words_list[count]
-        		words_list[count] = this_word[:-1]
-        		lwos.append(words_list[count])
-        		count = count+1
+        	char_count = 0
+        	base_word = "zxzxxxx"
+        	for char in w:
+        		if char in symbols or char in endlines:
+        			base_word = w[:-char_count]
+        			char_count = char_count + 1
+        		else:
+        			char_count = char_count + 1
+        	if base_word in d:
+        		d.append(w)
         	else:
-        		count = count+1
-        def neither_in(a,b,c):
-        	e = b+c
-        	if(a not in e):
-        		return True
-        	else:
-        		return False
-        neither = []
-        for n in words_list:
+        		pass
+
+        for n in words_list: #compare our user wordlist with our dictionary
         	if n in d:
-        		if neither_in(n, lwos, capital_words):
-        			neither.append(n),
-        		if(n in capital_words):
-        			n = n.capitalize()
-        			print(n)
-        		if(n in lwos):
-        			print(n+"."),
+        		print(n+" "),
         	else:
         		print(n+"(Mispelled)"),
 
@@ -70,9 +75,6 @@ def checked(request):
         	'n': n,
         	'words_list': words_list,
         	'd':d,
-        	'lwos':lwos,
-        	'capital_words':capital_words,
-        	'neither':neither,
         }
         return HttpResponse(template.render(context, request))
 
